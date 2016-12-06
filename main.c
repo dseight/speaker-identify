a38e3cbcb9271c40214fb20b9dc110ce15472f62
Dmitry Gerasimov <di.gerasimov@gmail.com>
2016-12-06 17:37:29 +0300

    First test version

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include <stdint.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <pulse/simple.h>
+
+#include "db.h"
+#include "mfcc.h"
 
 #define CHANNELS 1
 #define RATE 16000
@@ -14,7 +18,7 @@
  *  > 0 - size of recorded data
  *  < 0 - error
  */
-int record_sound(char *song_data, int time)
+int record_sound(int16_t **song_data, int time)
 {
     pa_simple *pulse_desc;
     pa_sample_spec parametrs;
@@ -42,11 +46,16 @@ int record_sound(char *song_data, int time)
     int size = RATE * CHANNELS * FORMAT * time / 8;
     int status;
 
-    song_data = (char *) malloc(size);
+    *song_data = (int16_t *) malloc(size);
+    if (*song_data == NULL) {
+        pa_simple_free(pulse_desc);
+        return -1;
+    }
 
-    status = pa_simple_read(pulse_desc, song_data, size, NULL);
+    status = pa_simple_read(pulse_desc, *song_data, size, NULL);
 
     if (status < 0) {
+        pa_simple_free(pulse_desc);
         return -1; /* error reading from audio record stream */
     }
 
@@ -57,8 +66,52 @@ int record_sound(char *song_data, int time)
 
 int main()
 {
-    char *data;
-    int size;
+    int16_t *data = NULL;
+    double mfcc[NCOEFFS];
 
-    size = record_sound(data, TIME);
+    if (init_mfcc() == -1) {
+        fprintf(stderr, "Error: MFCC initialization failed\n");
+        return 1;
+    }
+
+    printf("MFCC initializaton completed\n");
+
+    int size = record_sound(&data, TIME);
+    if (size < 0) {
+        fprintf(stderr, "Error: sound recording failed\n");
+        free_mfcc();
+        return 1;
+    }
+
+    size /= sizeof(int16_t);
+
+    printf("Sound recorded\n");
+
+    double *normalized_data = (double *) malloc(size * sizeof(double));
+    if (normalized_data == NULL) {
+        fprintf(stderr, "Error: memory allocation failed\n");
+        return 1;
+    }
+
+    double max = -100000.0;
+    for (int i = 0; i < size; ++i)
+        if (abs(data[i]) > max)
+            max = abs(data[i]);
+
+    for (int i = 0; i < size; ++i)
+        normalized_data[i] = (double) data[i] / max;
+
+    printf("Normalization completed\n");
+
+    get_mfcc(normalized_data, size, mfcc);
+    printf("MFCC computed\n");
+
+    // update_database(mfcc);
+    // printf("Database updated\n");
+
+    determine_name(mfcc);
+    printf("Name determined\n");
+
+    free_mfcc();
+    return 0;
 }
